e7221fb8dbde2bb2788b6f3bc292b6e6e755602f
KarolinaJankiewicz <125274679+KarolinaJankiewicz@users.noreply.github.com>
2023-04-10 19:52:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_compute_instance" "dareit-vm-ci" {
-  name         = "dareit-vm-tf-ci"
+resource "google_compute_instance" "dareit-vm-ci-1" {
+  name         = "dareit-vm-tf-ci-1"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
 
